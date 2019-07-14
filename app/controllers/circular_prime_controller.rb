7872b54285913cf3be10ae94ea00f8f761d303e6
benjamin de boissieu <bendeb@hotmail.com>
2019-07-14 21:12:18 +0200

    routes and show controller

--- app/controllers/circular_prime_controller.rb
+++ app/controllers/circular_prime_controller.rb
@@ -1,7 +1,30 @@
 class CircularPrimeController < ApplicationController
+
   def index
+    @circularNbs = CircularNb.all
   end
 
   def new
+    @circularNb = CircularNb.new
   end
+
+
+  def show
+    @circularNb = CircularNb.find(params[:id])
+  end
+
+  def create
+    @circularNb = CircularNb.new(circularNb_params)
+    if @circularNb.save
+      redirect_to circular_prime_path(@circularNb)
+    else
+      render :new
+    end
+  end
+
+  def circularNb_params
+    params.require(:circular_nb).permit(:value, :results)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'circular_prime/index'
-  get 'circular_prime/new'
+  resources :circular_prime
   root to: 'circular_prime#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
